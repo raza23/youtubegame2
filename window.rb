7758f023c8594faa7b391d7e4ec1c22624e3c763
Raza <rtshareef@gmail.com>
2020-03-07 19:45:00 -0500

    setting up window

---
+++ window.rb
@@ -0,0 +1,21 @@
+require 'gosu'
+
+class YoutubeGame2 < Gosu::Window
+    def initialize
+        #  * Sets up game window dimensions and the Screen Title
+        super 640,480
+        self.caption ='TMG'
+    end
+
+
+    def update
+    end
+
+    def draw
+    end
+
+
+end
+
+
+YoutubeGame2.new.show
