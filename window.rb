1f8548617f0ffb9932df35f36787bf36de31478c
Raza <rtshareef@gmail.com>
2020-03-08 15:29:56 -0400

    player class

--- window.rb
+++ window.rb
@@ -16,9 +16,40 @@ class YoutubeGame2 < Gosu::Window
     def draw
         @background_image.draw(0,0,0)
     end
-
-
 end
+
+class Player
+    def initialize
+        @image = Gosu::Image.new('images/pokemon_player.png')
+        @x = @y = @vel_x = @vel_y = @angle = 0.0 
+        @score = 0
+    end
+
+    def warp(x,y)
+        @x,@y = x,y
+    end
+
+    def turn_left
+        @angle -= 4.5
+    end
+
+    def turn_right
+        @angle += 4.5
+    end
+
+    def accelerate 
+        @vel_x +=Gose.offset_x(@angle,0.5)
+        @vel_y +=Gose.offset_y(@angle,0.5)
+    end
+
+    def move
+    end
+
+
+    def draw
+    end
+end
+
 
 
 YoutubeGame2.new.show
