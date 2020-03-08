d192f5ce4cd3dc4b1f8238f547f9a246fd2dce5e
Raza <rtshareef@gmail.com>
2020-03-08 15:42:30 -0400

    character on

--- window.rb
+++ window.rb
@@ -6,17 +6,40 @@ class YoutubeGame2 < Gosu::Window
         super 1600,1130
         self.caption ='Pokemon'
         @background_image = Gosu::Image.new("images/kanto1.png")
+        @player = Player.new
+        @player.warp(800,575)
     end
 
     # * Puts all game logic in update method
     def update
+        if Gosu.button_down? Gosu::KB_LEFT or Gosu::button_down? Gosu::GP_LEFT
+            @player.turn_left
+        end
+
+        if Gosu.button_down? Gosu::KB_RIGHT or Gosu::button_down? Gosu::GP_RIGHT
+            @player.turn_right
+        end
+
+        if Gosu.button_down? Gosu::KB_UP or Gosu::button_down? Gosu::GP_BUTTON_0
+            @player.accelerate
+        end
+
+        @player.move
 
     end
 
     def draw
+        @player.draw
         @background_image.draw(0,0,0)
     end
 end
+
+
+
+
+
+
+
 
 class Player
     def initialize
@@ -38,15 +61,23 @@ class Player
     end
 
     def accelerate 
-        @vel_x +=Gose.offset_x(@angle,0.5)
-        @vel_y +=Gose.offset_y(@angle,0.5)
+        @vel_x += Gosu.offset_x(@angle,0.5)
+        @vel_y += Gosu.offset_y(@angle,0.5)
     end
 
     def move
+        @x += @vel_x
+        @y += @vel_y
+        @x %= 640
+        @y %= 480
+
+        @vel_x *= 0.95
+        @vel_y *= 0.95
     end
 
 
     def draw
+        @image.draw_rot(@x,@y,1,@angle)
     end
 end
 
