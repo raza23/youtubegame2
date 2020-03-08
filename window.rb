642392910afc180e08a800f1741585c7e3fde226
Raza <rtshareef@gmail.com>
2020-03-08 15:10:48 -0400

    adding background image

--- window.rb
+++ window.rb
@@ -3,15 +3,18 @@ require 'gosu'
 class YoutubeGame2 < Gosu::Window
     def initialize
         #  * Sets up game window dimensions and the Screen Title
-        super 640,480
-        self.caption ='TMG'
+        super 1600,1130
+        self.caption ='Pokemon'
+        @background_image = Gosu::Image.new("images/kanto1.png")
     end
 
-
+    # * Puts all game logic in update method
     def update
+
     end
 
     def draw
+        @background_image.draw(0,0,0)
     end
 
 
