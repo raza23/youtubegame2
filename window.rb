796091b19568acb3db50d66a189cdce6b4152616
Raza <rtshareef@gmail.com>
2020-03-09 08:30:54 -0400

    finished

--- window.rb
+++ window.rb
@@ -1,5 +1,9 @@
 require 'gosu'
 
+module ZOrder
+    BACKGROUND,POKEBALLS,PLAYER,UI = *0..3
+end
+
 class YoutubeGame2 < Gosu::Window
     def initialize
         #  * Sets up game window dimensions and the Screen Title
@@ -8,6 +12,9 @@ class YoutubeGame2 < Gosu::Window
         @background_image = Gosu::Image.new("images/kanto1.png")
         @player = Player.new
         @player.warp(400,575)
+        @pokeball = Gosu::Image.load_tiles("images/pokeball.png",200,200)
+        @pokeballs = Array.new
+        @font = Gosu::Font.new(40)
     end
 
     # * Puts all game logic in update method
@@ -25,12 +32,20 @@ class YoutubeGame2 < Gosu::Window
         end
 
         @player.move
+        @player.catch(@pokeballs)
+
+        if rand(100) < 4 and @pokeballs.size < 25
+            @pokeballs.push(PokeBall.new(@pokeball))
+        end
 
     end
 
     def draw
         @player.draw
-        @background_image.draw(0,0,0)
+        @background_image.draw(0,0,ZOrder::BACKGROUND)
+        @pokeballs.each{|ball| ball.draw}
+        @font.draw_text("Score: #{@player.score}", 10, 10, ZOrder::UI, 1.0, 1.0, Gosu::Color::BLACK)
+
     end
 end
 
@@ -41,7 +56,10 @@ end
 
 
 
+
 class Player
+
+    attr_reader :score
     def initialize
         @image = Gosu::Image.new('images/pokemon_player.png')
         @x = @y = @vel_x = @vel_y = @angle = 0.0 
@@ -79,8 +97,47 @@ class Player
     def draw
         @image.draw_rot(@x,@y,1,@angle)
     end
+
+    def score 
+        @score
+    end
+
+    def catch(pokeballs)
+        pokeballs.reject! do |ball| 
+            if Gosu.distance(@x,@y, ball.x,ball.y) < 35
+                @score += 10
+                true 
+            else
+                false
+            end
+        end
+    end
+
+    
+
 end
 
+
+
+class PokeBall
+    attr_reader :x, :y
+    def initialize(animation)
+        @animation = animation
+        @color = Gosu::Color::BLACK.dup
+        @color.red = rand(256-40)+40
+        @color.green = rand(256-40)+40
+        @color.blue = rand(256-40)+40
+        @x = rand * 1600
+        @y = rand * 1130
+
+    end
+
+    def draw
+        img = @animation[Gosu.milliseconds / 100 % @animation.size]
+        img.draw(@x - img.width / 2.0, @y - img.height / 2.0, ZOrder::POKEBALLS,1,1,@color,:add)
+    end
+
+end
 
 
 YoutubeGame2.new.show
